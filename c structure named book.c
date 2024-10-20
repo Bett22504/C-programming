821e1c5ce5a025a5c6ebbbfd65fa81270149a4dc
CT101/G/22504/24 Immanuel Bett <kipronoimmanuel84@gmail.com>
2024-10-20 11:32:26 +0300

    Add files via upload

---
+++ c structure named book.c
@@ -0,0 +1,28 @@
+//C program to define a structure named book
+/*Aurthor:Immanuel Bett
+Reg no:CT101/G/22504*/
+#include <stdio.h>
+#include <string.h>
+struct book{
+char title[30];
+char aurthor[30];
+int publication_year;
+char ISBN[13];
+float price;
+}book;
+
+int main(){
+strcpy(book.title,"Introduction_to_C_programing");
+strcpy(book.aurthor,"John_Smith");
+book.publication_year=2022;
+strcpy(book.ISBN,"9780131103627");
+book.price=49.99;
+
+printf("Title:%s\n",book.title);
+printf("Aurthor:%s\n",book.aurthor);
+printf("Publication year:%d\n",book.publication_year);
+printf("ISBN:%s\n",book.ISBN);
+printf("Price:%s\n",book.price);
+
+return 0;
+}
